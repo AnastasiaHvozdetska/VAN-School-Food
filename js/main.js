8d744f1259ecc345608c6632a5853692665c2ead
AnastasiaHvozdetska <anastasia9297@gmail.com>
2018-03-22 18:02:22 +0200

    added pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-$(document).ready(function(){$(".slider").slick({dots:!0,arrows:!1,slidesToShow:3,slidesToScroll:3,autoplay:!0,autoplaySpeed:2e3}),$("#phone-number").mask("(999) 999-9999")});
+$(document).ready(function () {
+	//slider
+	$('.slider').slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+
+	$('.checkbox').on('click', function () {
+		$('.checkbox').css('background-color', '#303030');
+		$(this).css('background-color', '#ff8303');
+		$('label').css('color', '#fff');
+		$(this).find('label').css('color', '#303030');
+	});
+
+	$('.case').on('click', function () {
+		$(this).parent().next('.btn-group-step').show();
+	});
+	
+	$(function(){
+		var heightMenu = $('.account-content').height();
+		$('.left-menu-wrapper').height(heightMenu);
+	});
+	
+});
